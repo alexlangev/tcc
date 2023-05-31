9827cfec6ed4612b2e77eb1b53d0c875b4fc4556
Alex Langevin <alexlangev@gmail.com>
2023-05-31 11:13:27 -0400

    links to leetcode and smart contract engineer

--- pages/daily-grind/index.jsx
+++ pages/daily-grind/index.jsx
@@ -12,7 +12,38 @@ export default function DailyGrindPage() {
       <DesktopSpacer height={32} />
       <PageHeader title={TITLE} sub={SUBTITLE} />
       <DesktopSpacer height={64} />
-      <Category>Capture The Flag Challenges</Category>
+
+      <CategoryName>Coding Challenges</CategoryName>
+
+      <Content>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={"https://leetcode.com/AlexLangev/"}
+        >
+          <h3>LeetCode</h3>
+          <Subtitle>{`One of the leading platform for coding skills. It offers a plethora of 
+          algorithmic challenges that range from data structures to databases.`}</Subtitle>
+        </a>
+      </Content>
+
+      <Content>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={"https://www.smartcontract.engineer/leaderboard"}
+        >
+          <h3>Smart Contract Engineer</h3>
+          <Subtitle>
+            The LeetCode for Solidity engineers. Look for{" "}
+            <em>The Coding Canuck</em> in the <strong>Top 100</strong>{" "}
+            leaderboard.
+          </Subtitle>
+        </a>
+      </Content>
+
+      <CategoryName>Capture The Flag Challenges</CategoryName>
+
       <Content>
         <Link href={"daily-grind/ethernaut"}>
           <h3>Ethernaut</h3>
@@ -31,7 +62,7 @@ const Subtitle = styled.p`
   color: ${COLORS.sub};
 `;
 
-const Category = styled.h2`
+const CategoryName = styled.h2`
   margin-bottom: 32px;
 `;
 
